2fe98c6d7989274d56ef98a4d01d0660402ecb82
Anders Evenrud <andersevenrud@gmail.com>
2018-07-12 23:02:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+<p align="center">
+  <img alt="OS.js Logo" src="https://raw.githubusercontent.com/os-js/gfx/master/logo-big.png" />
+</p>
+
+[OS.js](https://www.os-js.org/) is an [open-source](https://raw.githubusercontent.com/os-js/OS.js/master/LICENSE) desktop implementation for your browser with a fully-fledged window manager, Application APIs, GUI toolkits and filesystem abstraction.
+
+[![Community](https://img.shields.io/badge/join-community-green.svg)](https://community.os-js.org/)
+[![Donate](https://img.shields.io/badge/liberapay-donate-yellowgreen.svg)](https://liberapay.com/os-js/)
+[![Donate](https://img.shields.io/badge/paypal-donate-yellow.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=andersevenrud%40gmail%2ecom&lc=NO&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted)
+[![Support](https://img.shields.io/badge/patreon-support-orange.svg)](https://www.patreon.com/user?u=2978551&ty=h&u=2978551)
+
+# OS.js v3 Google API Provider
+
+This is the Google API Provider for OS.js v3.
+
+## Installation
+
+In your server initialization script:
+
+```
+const GapiServiceProvider = require('@osjs/gapi-provider');
+
+core.register(GapiServiceProvider, {
+  args: {
+    // These are set for you by default
+    src: 'https://apis.google.com/js/api.js',
+    libraries: 'client:auth2',
+    timeout: 30000,
+
+    // You have to define these
+    client: {
+      apiKey: '',
+      clientId: '',
+      discoveryDocs: [],
+      scope: []
+    }
+  }
+});
+```
+
+## Usage
+
+For example in an application:
+```
+const onSignedIn = () => {
+  const gapi = core.make('google/api'); // or 'window.gapi'
+
+  // Do whatever you want here
+};
+
+// Get the provider
+const osjsgapi = core.make('osjs/gapi');
+
+// Remove our event on quit
+proc.on('destroy', () => osjsgapi.off('signed-in', onSignedIn))
+
+// Attach the logged in event listener
+osjsgapi.on('signed-in', onSignedIn);
+
+// Try to log in. This will fire the 'signed-in' event when successful.
+osjsgapi.login();
+```
---
+++ index.js
@@ -0,0 +1,234 @@
+/*
+ * OS.js - JavaScript Cloud/Web Desktop Platform
+ *
+ * Copyright (c) 2011-2018, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+
+import {ServiceProvider, EventHandler} from '@osjs/common';
+
+/*
+ * Alias
+ */
+const gapiAuthInstance = () => window.gapi.auth2.getAuthInstance();
+
+/*
+ * Creates gapi client instance
+ */
+const createGapiClient = options => {
+  const opts = Object.assign({
+    apiKey: null,
+    clientId: null,
+    discoveryDocs: [],
+    scope: []
+  }, options);
+
+  opts.scope = opts.scope.join(' ');
+
+  // If these are not correct we just get some lame 404s from google
+
+  if (!opts.apiKey || !opts.clientId) {
+    throw new Error('gapi client requires apiKey and clientId');
+  }
+
+  if (!opts.discoveryDocs.length) {
+    throw new Error('gapi client requires discoveryDocs');
+  }
+
+  if (!opts.scope.length) {
+    throw new Error('gapi client requires scope');
+  }
+
+  return window.gapi.client.init(opts);
+};
+
+/*
+ * Loads the gapi client
+ */
+const loadGapiClient = (libraries, timeout) => new Promise((resolve, reject) => {
+  if (libraries) {
+    console.debug('Loading gapi client with', libraries);
+
+    window.gapi.load(libraries, {
+      timeout,
+      callback: () => resolve(),
+      onerror: err => reject(new Error(err)),
+      ontimeout: () => reject(new Error('Timed Out')) // FIXME
+    });
+  } else {
+    // Not spesifying just makes 404s at google
+    reject(new Error('gapi load requires libraries'));
+  }
+});
+
+/**
+ * Google API Service Provider
+ * @desc Provides gapi
+ */
+export class GapiServiceProvider extends ServiceProvider {
+
+  constructor(core, options) {
+    super(core, Object.assign({
+      src: 'https://apis.google.com/js/api.js',
+      timeout: 30000,
+      libraries: 'client:auth2',
+      client: {}
+    }, options));
+
+    this.signedIn = false;
+    this.loaded = false;
+    this.bus = new EventHandler('gapi');
+  }
+
+  /**
+   * Destroys the service provider
+   */
+  destroy() {
+    this.tray = this.tray.destroy();
+    this.bus = this.bus.destroy();
+  }
+
+  /**
+   * Initializes the service provider
+   * @desc Exposes and binds api stuff
+   */
+  init() {
+    this.core.singleton('google/api', () => {
+      if (!('gapi' in window)) {
+        throw new Error('gapi was not loaded');
+      }
+
+      return window.gapi;
+    });
+
+    this.core.singleton('osjs/gapi', () => ({
+      logout: () => this.logout(),
+      login: () => this.login(),
+      on: (...args) => this.bus.on(...args),
+      off: (...args) => this.bus.off(...args)
+    }));
+
+    this.bus.on('signed-in', () => {
+      this.core.make('osjs/notification', {
+        icon: require('./logo.svg'),
+        title: 'Google API',
+        message: 'You have signed on to Google'
+      });
+    });
+
+    this.bus.on('signed-out', () => (this.loaded = false));
+  }
+
+  /**
+   * Start the service provider
+   */
+  start() {
+    this.createTray();
+
+    return this.createApi();
+  }
+
+  /**
+   * Creates tray icon
+   */
+  createTray() {
+    this.tray = this.core.make('osjs/tray').create({
+      title: 'Google API',
+      icon: require('./logo.svg')
+    }, ev => {
+      this.core.make('osjs/contextmenu').show({
+        position: ev.target,
+        menu: [{
+          label: 'Scopes',
+          items: this.options.client.scope.map(s => ({
+            label: s
+          }))
+        }, {
+          label: 'Sign In',
+          disabled: this.signedIn,
+          onclick: () => this.login()
+        }, {
+          label: 'Sign Out',
+          disabled: !this.signedIn,
+          onclick: () => this.logout()
+        }]
+      });
+    });
+  }
+
+  /**
+   * Creates google api
+   */
+  createApi() {
+    const {src, libraries, timeout, client} = this.options;
+    const {script} = this.core.make('osjs/dom');
+    const {$resourceRoot} = this.core;
+
+    return script($resourceRoot, src)
+      .then(() => loadGapiClient(libraries, timeout))
+      .then(() => createGapiClient(client))
+      .then(() => this.listen());
+  }
+
+  /**
+   * Sets up the gapi auth listening
+   */
+  listen() {
+    const emit = () => this.bus.emit(this.signedIn ? 'signed-in' : 'signed-out');
+    if (!this.loaded) {
+      gapiAuthInstance().isSignedIn.listen(s => {
+        this.signedIn = s;
+
+        emit();
+      });
+
+      this.signedIn = gapiAuthInstance().isSignedIn.get();
+
+      emit();
+    }
+
+    this.loaded = true;
+  }
+
+  /**
+   * Perform login
+   */
+  login() {
+    if (!this.signedIn) {
+      gapiAuthInstance().signIn();
+    }
+  }
+
+  /**
+   * Perform logout
+   */
+  logout() {
+    if (this.signedIn) {
+      gapiAuthInstance().signOut();
+    }
+  }
+}
+
